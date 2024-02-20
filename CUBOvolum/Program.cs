298a65b8186add9839f13aa4de17dccd7b5b21c6
CarlosMtzV <159225079+CarlosMtzV@users.noreply.github.com>
2024-02-20 00:36:56 -0600

    CubeVolum

---
+++ CUBOvolum/Program.cs
@@ -0,0 +1,7 @@
+﻿using CUBOvolum;
+
+VolCub miCubo = new VolCub(5);
+
+double volumen = miCubo.CalcularVolumen();
+
+Console.WriteLine("Volume of cube with side length  " + miCubo.Lado + "  is:  " + volumen);
---
+++ CUBOvolum/VolCub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUBOvolum
+{
+    internal class VolCub
+    {
+        public double Lado { get; set; }
+
+        // Constructor
+        public VolCub(double lado)
+        {
+            Lado = lado;
+        }
+
+        // Método para calcular el volumen del cubo
+        public double CalcularVolumen()
+        {
+            // El volumen de un cubo es igual al lado elevado al cubo
+            return Math.Pow(Lado, 3);
+        }
+    }
+}
